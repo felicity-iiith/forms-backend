eaac415de090755e7663287dea2c93c5f922d2fb
Megh Parikh <meghprkh@gmail.com>
2018-01-11 19:31:38 +0530

    Use sequelize-json to avoid errors as mysql is not deserializing JSON for some reason

--- src/models/Response.js
+++ src/models/Response.js
@@ -1,11 +1,12 @@
 import Sequelize from "sequelize";
+import JsonField from "sequelize-json";
 import db from "./db";
 
 import User from "./User";
 
 const Response = db.define("response", {
   formslug: Sequelize.STRING,
-  response: Sequelize.JSON,
+  response: JsonField(db, "response", "response", { type: Sequelize.TEXT }),
   payment_request_id: {
     // form: account|payment_request_id
     type: Sequelize.STRING,
